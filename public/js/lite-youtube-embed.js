3846e535074ebefebcdd3c3c46614344d05aa62d
KingAshu22 <ashishprasad9833@gmail.com>
2023-09-10 03:13:12 -0700

    updated the Video Playing problem

--- public/js/lite-youtube-embed.js
+++ public/js/lite-youtube-embed.js
@@ -1,174 +1,50 @@
-class LiteYTEmbed extends HTMLElement {
-    connectedCallback() {
-        // Check if the video source is a YouTube link or an AWS video link
-        const videoSource = this.getAttribute('videosource');
-        if (videoSource === 'youtube') {
-            this.classList.add('youtube-video');
-            this.setupYouTubeVideo();
-        } else if (videoSource === 'aws') {
-            this.setupAWSVideo();
-        }
+function labnolIframe(div) {
+    var videoSource = div.getAttribute('videosource');
+    var videoId = div.dataset.id;
+
+    if (videoSource === 'youtube') {
+        // Create a YouTube iframe
+        var youtubeIframe = document.createElement('iframe');
+        youtubeIframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1');
+        youtubeIframe.setAttribute('frameborder', '0');
+        youtubeIframe.setAttribute('allowfullscreen', '1');
+        youtubeIframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+
+        // Replace the div with the YouTube iframe
+        div.parentNode.replaceChild(youtubeIframe, div);
+    } else if (videoSource === 'aws') {
+        // Create an AWS video player (modify this part based on your AWS video player implementation)
+        // Example: You can create an HTML5 video element for AWS videos
+        var awsVideo = document.createElement('video');
+        awsVideo.setAttribute('src', videoId);
+        awsVideo.setAttribute('controls', 'controls');
+        awsVideo.style.maxWidth = '100%'; // Ensure the video stays within its container
+        
+        // Replace the div with the AWS video element
+        div.parentNode.replaceChild(awsVideo, div);
     }
-
-    // Method to set up a YouTube video
-    setupYouTubeVideo() {
-        this.videoId = this.getAttribute('videoid');
-
-        let playBtnEl = this.querySelector('.lty-playbtn');
-        // A label for the button takes priority over a [playlabel] attribute on the custom-element
-        this.playLabel = (playBtnEl && playBtnEl.textContent.trim()) || this.getAttribute('playlabel') || 'Play';
-
-        /**
-         * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
-         *
-         * See https://github.com/paulirish/lite-youtube-embed/blob/master/youtube-thumbnail-urls.md
-         *
-         * TODO: Do the sddefault->hqdefault fallback
-         *       - When doing this, apply referrerpolicy (https://github.com/ampproject/amphtml/pull/3940)
-         * TODO: Consider using webp if supported, falling back to jpg
-         */
-        if (!this.style.backgroundImage) {
-          this.style.backgroundImage = `url("https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg")`;
-        }
-
-        // Set up play button, and its visually hidden label
-        if (!playBtnEl) {
-            playBtnEl = document.createElement('button');
-            playBtnEl.type = 'button';
-            playBtnEl.classList.add('lty-playbtn');
-            this.append(playBtnEl);
-        }
-        if (!playBtnEl.textContent) {
-            const playBtnLabelEl = document.createElement('span');
-            playBtnLabelEl.className = 'lyt-visually-hidden';
-            playBtnLabelEl.textContent = this.playLabel;
-            playBtnEl.append(playBtnLabelEl);
-        }
-        playBtnEl.removeAttribute('href');
-
-        // On hover (or tap), warm up the TCP connections we're (likely) about to use.
-        this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {once: true});
-
-        // Once the user clicks, add the real iframe and drop our play button
-        // TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
-        //   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
-        this.addEventListener('click', this.addIframe);
-
-        // Chrome & Edge desktop have no problem with the basic YouTube Embed with ?autoplay=1
-        // However Safari desktop and most/all mobile browsers do not successfully track the user gesture of clicking through the creation/loading of the iframe,
-        // so they don't autoplay automatically. Instead we must load an additional 2 sequential JS files (1KB + 165KB) (un-br) for the YT Player API
-        // TODO: Try loading the the YT API in parallel with our iframe and then attaching/playing it. #82
-        this.needsYTApiForAutoplay = navigator.vendor.includes('Apple') || navigator.userAgent.includes('Mobi');
-    }
-
-    // Method to set up an AWS video
-    setupAWSVideo() {
-        // Retrieve the AWS video URL from the attribute
-        const awsVideoUrl = this.getAttribute('videoid');
-
-        // Create an <video> element to display the AWS video
-        const videoEl = document.createElement('video');
-        videoEl.src = awsVideoUrl;
-        videoEl.controls = true; // Display video controls
-        videoEl.style.width = '100%'; // Set the video width
-
-        this.append(videoEl);
-    }
-
-    /**
-     * Add a <link rel={preload | preconnect} ...> to the head
-     */
-    static addPrefetch(kind, url, as) {
-        const linkEl = document.createElement('link');
-        linkEl.rel = kind;
-        linkEl.href = url;
-        if (as) {
-            linkEl.as = as;
-        }
-        document.head.append(linkEl);
-    }
-
-    /**
-     * Begin pre-connecting to warm up the iframe load
-     * Since the embed's network requests load within its iframe,
-     *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
-     * So, the best we can do is warm up a few connections to origins that are in the critical path.
-     *
-     * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
-     * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
-     */
-    static warmConnections() {
-        if (LiteYTEmbed.preconnected) return;
-
-        // The iframe document and most of its subresources come right off youtube.com
-        LiteYTEmbed.addPrefetch('preconnect', 'https://www.youtube-nocookie.com');
-        // The botguard script is fetched off from google.com
-        LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
-
-        // Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
-        LiteYTEmbed.addPrefetch('preconnect', 'https://googleads.g.doubleclick.net');
-        LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
-
-        LiteYTEmbed.preconnected = true;
-    }
-
-    fetchYTPlayerApi() {
-        if (window.YT || (window.YT && window.YT.Player)) return;
-
-        this.ytApiPromise = new Promise((res, rej) => {
-            var el = document.createElement('script');
-            el.src = 'https://www.youtube.com/iframe_api';
-            el.async = true;
-            el.onload = _ => {
-                YT.ready(res);
-            };
-            el.onerror = rej;
-            this.append(el);
-        });
-    }
-
-    async addYTPlayerIframe(params) {
-        this.fetchYTPlayerApi();
-        await this.ytApiPromise;
-    
-        const videoPlaceholderEl = document.createElement('div');
-        this.append(videoPlaceholderEl);
-    
-        const paramsObj = Object.fromEntries(params.entries());
-    
-        new YT.Player(videoPlaceholderEl, {
-            width: '100%',
-            height: 'auto', // Set height to 'auto' for dynamic sizing
-            videoId: this.videoId,
-            playerVars: paramsObj,
-            events: {
-                'onReady': event => {
-                    event.target.playVideo();
-                }
-            }
-        });
-    }
-    
-    async addIframe() {
-        if (this.classList.contains('lyt-activated')) return;
-        this.classList.add('lyt-activated');
-    
-        const params = new URLSearchParams(this.getAttribute('params') || []);
-        params.append('autoplay', '1');
-        params.append('playsinline', '1');
-    
-        if (this.needsYTApiForAutoplay) {
-            return this.addYTPlayerIframe(params);
-        }
-    
-        const iframeEl = document.createElement('iframe');
-        iframeEl.width = '100%';
-        iframeEl.height = 'auto'; // Set height to 'auto' for dynamic sizing
-        // ... (rest of the iframe setup)
-    
-        this.append(iframeEl);
-        iframeEl.focus();
-    }    
 }
-// Register custom element
-customElements.define('lite-youtube', LiteYTEmbed);
+
+function initYouTubeVideos() {
+    var playerElements = document.querySelectorAll('.youtube-player');
+    for (var n = 0; n < playerElements.length; n++) {
+        var div = playerElements[n];
+        div.onclick = function () {
+            labnolIframe(this);
+        };
+
+        // Create a thumbnail image for YouTube videos
+        if (div.getAttribute('videosource') === 'youtube') {
+            var thumbnailImg = document.createElement('img');
+            thumbnailImg.src = 'https://img.youtube.com/vi/' + div.dataset.id + '/hqdefault.jpg';
+            div.appendChild(thumbnailImg);
+        }
+
+        // Create a play button overlay
+        var playButton = document.createElement('div');
+        playButton.setAttribute('class', 'play-button');
+        div.appendChild(playButton);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initYouTubeVideos);
