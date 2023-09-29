cb165f979466eb492d600f52a3ba5aca76e1c6e1
KingAshu22 <ashishprasad9833@gmail.com>
2023-09-29 17:23:19 +0530

    updated book singers for event 301 redirections link

--- redirections.js
+++ redirections.js
@@ -707,7 +707,7 @@ const redirections = [
     newUrl: '/blog/book-singers-in-mumbai'
   },
   {
-    oldUrl: 'book-singer-for-event',
+    oldUrl: '/book-singer-for-event',
     newUrl: '/blog/book-singers-for-event'
   },
   {
