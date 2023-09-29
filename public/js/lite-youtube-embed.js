2599aa24ff2002f596929eb6d069fa6ca90a73d5
KingAshu22 <ashishprasad9833@gmail.com>
2023-09-29 11:15:46 +0530

    made video div responsive

--- public/js/lite-youtube-embed.js
+++ public/js/lite-youtube-embed.js
@@ -5,7 +5,13 @@ function labnolIframe(div) {
     if (videoSource === 'youtube') {
         // Create a YouTube iframe
         var youtubeIframe = document.createElement('iframe');
+        var thumbnailImg = div.querySelector('img'); // Get the thumbnail image
+        var width = thumbnailImg.width;
+        var height = thumbnailImg.height - 50;
+        
         youtubeIframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1');
+        youtubeIframe.setAttribute('width', width);
+        youtubeIframe.setAttribute('height', height);
         youtubeIframe.setAttribute('frameborder', '0');
         youtubeIframe.setAttribute('allowfullscreen', '1');
         youtubeIframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
@@ -18,7 +24,7 @@ function labnolIframe(div) {
         var awsVideo = document.createElement('video');
         awsVideo.setAttribute('src', videoId);
         awsVideo.setAttribute('controls', 'controls');
-        awsVideo.style.maxWidth = '100%'; // Ensure the video stays within its container
+        awsVideo.style.width = '100%'; // Set the width to 100% for full width
         
         // Replace the div with the AWS video element
         div.parentNode.replaceChild(awsVideo, div);
